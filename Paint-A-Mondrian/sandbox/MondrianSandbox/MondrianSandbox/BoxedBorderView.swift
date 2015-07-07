b1817a014363e932ae925576f8a3f2d36d2bd965
seanreed1111 <seanreed1111@gmail.com>
2015-07-07 09:32:49 -0400

    draws boxes (UIViews) with borders

---
+++ Paint-A-Mondrian/sandbox/MondrianSandbox/MondrianSandbox/BoxedBorderView.swift
@@ -0,0 +1,15 @@
+//
+//  BoxedBorderView.swift
+//  MondrianSandbox
+//
+//  Created by Sean Reed on 7/7/15.
+//  Copyright (c) 2015 Sean Reed. All rights reserved.
+// Xcode 6.4, Swift 1.2
+//
+
+import UIKit
+import QuartzCore
+
+class BoxedBorderView : UIView {
+    //
+}
--- Paint-A-Mondrian/sandbox/MondrianSandbox/MondrianSandbox/ViewController.swift
+++ Paint-A-Mondrian/sandbox/MondrianSandbox/MondrianSandbox/ViewController.swift
@@ -7,17 +7,28 @@
 //
 
 import UIKit
+import QuartzCore
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redBoxView: BoxedBorderView!
+    
+    @IBOutlet weak var greenBoxView: BoxedBorderView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //redBoxView and greenBoxView were set up in IB
+        
+        // add a black border to them in viewDidLoad
+        
+        redBoxView.layer.borderColor = UIColor.blackColor().CGColor
+        redBoxView.layer.borderWidth = 2.0
+        
+        greenBoxView.layer.borderColor = UIColor.blackColor().CGColor
+        greenBoxView.layer.borderWidth = 2.0
     }
 
 
