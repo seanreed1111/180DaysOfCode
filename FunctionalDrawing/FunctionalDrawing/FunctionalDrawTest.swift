fcc71c72f40e3ca3e12e6ca7b47b33f6ad170be7
seanreed1111 <seanreed1111@gmail.com>
2015-07-21 13:00:00 -0400

    finishes definition and extension of Diagram enum. Function fit not working

--- FunctionalDrawing/FunctionalDrawing/FunctionalDrawTest.swift
+++ FunctionalDrawing/FunctionalDrawing/FunctionalDrawTest.swift
@@ -10,8 +10,14 @@ import Foundation
 import UIKit
 import CoreGraphics
 
-class FunctionalDrawTest :NSObject {
+struct Vector2D {
+    let x: CGFloat
+    let y:CGFloat
     
+    var point: CGPoint {return CGPointMake(x,y)}
+    var size: CGSize { return CGSizeMake(x, y)}
+}
+
     enum Primitive{
         case Ellipse
         case Rectangle
@@ -29,6 +35,36 @@ class FunctionalDrawTest :NSObject {
     enum Attribute{
         case FillColor(UIColor)
     }
-    
-}
 
+
+extension Diagram {
+    var size: CGSize {
+        switch self {
+            case .Prim(let size, _):
+                return size
+            case .Attributed(_, let x):
+                return x.unbox.size
+            case .Beside(let l, let r):
+                let sizeL = l.unbox.size
+                let sizeR = r.unbox.size
+                return CGSizeMake(sizeL.width + sizeR.width, max(sizeL.height, sizeR.height))
+            case .Below(let l, let r):
+                let sizeL = l.unbox.size
+                let sizeR = r.unbox.size
+                return CGSizeMake(max(sizeL.width,sizeR.width), sizeL.height + sizeR.height)
+            case .Align(_, let r):
+                return r.unbox.size
+        }
+    }
+    
+    // this function is totally busted
+    func fit(alignment: Vector2D, inputSize: CGSize, rect: CGRect) -> CGRect{
+
+        let scale = min(rect.size.width / inputSize.width, rect.size.height / inputSize.height)
+        let size = CGSizeMake(scale * inputSize.width, scale * inputSize.height)
+        let space = CGSizeMake(alignment.size.width * (size.width - rect.size.width), alignment.size.height * (size.height - rect.size.height))
+        
+        return CGRectZero
+        
+    }
+}
